6bb476c1c814c799cacf6bea00e0d7c27b333f41
vishakh <kmsree1990@gmail.com>
2018-01-21 01:28:39 +0530

    debug False Settings.py

--- mynewsite/settings.py
+++ mynewsite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'rw!07f4y5#=9l9=li05-=hxd2v$e7^am2%3ez9=5-)6=0c=*w_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
 ]
